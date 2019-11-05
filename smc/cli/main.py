24db00bba68a82c2e799146b49210920c139e990
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-11-06 02:48:25 +0700

    Update cli can read input file

--- smc/cli/main.py
+++ smc/cli/main.py
@@ -1,2 +1,12 @@
+import argparse
+
 def main():
-  print('hello world')
+  parser = argparse.ArgumentParser(description='SMC Simulator')
+  parser.add_argument('inputfile', help='SMC source file')
+
+  args = parser.parse_args()
+
+  text = open(args.inputfile, 'r').read()
+  
+  # TODO: compile and simulator input file
+  print(text)
