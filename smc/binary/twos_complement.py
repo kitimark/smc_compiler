2a0e4648a14794427aa60ab694af2c02760aaf8f
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-11-07 05:25:08 +0700

    Update auto param

--- smc/binary/twos_complement.py
+++ smc/binary/twos_complement.py
@@ -1,11 +1,11 @@
 from bitstring import Bits, ByteStore, CreationError
 
 class TwosComplement(Bits):
-  def __init__(self, bits=None, **kwargs):
+  def __init__(self, auto=None, bits=None, **kwargs):
     if not isinstance(self._datastore, ByteStore):
       self._ensureinmemory()
       
-  def __new__(cls, bits=None, **kwargs):
+  def __new__(cls, auto=None, bits=None, **kwargs):
     """
       Scope of **kwargs with keys:
       bin -- binary string representation, e.g. '0b001010'.
@@ -14,16 +14,33 @@ class TwosComplement(Bits):
       int -- a signed integer.
     """
     x = super(TwosComplement, cls).__new__(cls)
-
     x._check_kwargs(**kwargs)
-
-    y = Bits.__new__(TwosComplement, length=bits, **kwargs)
+    # y = Bits.__new__(TwosComplement, length=bits, **kwargs)
+    y = TwosComplement._initialise_object(auto, length=bits, **kwargs)
     x._datastore = ByteStore(y._datastore._rawarray[:],
                               y._datastore.bitlength,
                               y._datastore.offset)
     return x
+  
+  @classmethod
+  def _initialise_object(cls, auto, length, **kwargs):
+    try :
+      if isinstance(auto, int):
+        return Bits.__new__(cls, length=length, int=auto)
+      if isinstance(auto, str):
+        integer = int(auto, 0)
+        return Bits.__new__(cls, length=length, int=integer)
+      if auto is not None: 
+        raise ValueError
+    except ValueError: 
+      raise CreationError(f"Unreconginised keyword '{auto}' used to initialise.")
+    
+    return Bits.__new__(cls, length=length, **kwargs)
 
   def _check_kwargs(self, **kwargs):
+    if not kwargs: 
+      return 
+
     k, v = kwargs.popitem()
     try:
       self.keys.index(k)
--- tests/binary/test_twos_complement.py
+++ tests/binary/test_twos_complement.py
@@ -8,5 +8,9 @@ class TestTwosComplement(unittest.TestCase):
 
     actual = TwosComplement(int=5, bits=32)
     self.assertEqual(actual.bin, '00000000000000000000000000000101')
+    self.assertEqual(TwosComplement(bits=32).int, 0)
+    self.assertEqual(TwosComplement('0b1010', bits=32).int, 10)
 
+    self.assertRaises(CreationError, TwosComplement, ['exception'])
+    self.assertRaises(CreationError, TwosComplement, 'exception')
     self.assertRaises(CreationError, TwosComplement, value=5, bits=32)
