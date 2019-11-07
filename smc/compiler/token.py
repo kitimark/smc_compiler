dd6a17d95a6499b6265c5e5335f05180b649dac9
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-11-07 09:52:48 +0700

    Initialize token object

---
+++ smc/compiler/token.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+class TokenType(Enum):
+  # block of reserved words
+  ADD      = 'add'
+  NAND     = 'nand'
+  LW       = 'lw'
+  SW       = 'sw'
+  BEQ      = 'beq'
+  JALR     = 'jalr'
+  HALT     = 'halt'
+  NOOP     = 'noop'
+  FILL     = '.fill'  
+  # misc
+  INTERGER = 'INTERGER'
+  LABEL    = 'LABEL'
+  EOL      = 'EOL' # End of line
+  EOF      = 'EOF' # End of file
+
+class Token(object):
+  def __init__(self, type, value, line, column):
+    self.type = type
+    self.value = value
+    self.line = line
+    self.column = column
+
+  def __str__(self):
+    return f'Token({self.type}, {self.value}, position: ({self.line}:{self.column}))'
+  
+  @staticmethod
+  def RESERVED_KEYWORDS():
+    tt_list = list(TokenType)
+    start_index = tt_list.index(TokenType.ADD)
+    end_index = tt_list.index(TokenType.FILL)
+    reserved_keywords = {
+      token_type.value: token_type for token_type in tt_list[start_index: end_index + 1]
+    }
+    return reserved_keywords
