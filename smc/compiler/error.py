57f3b765f07b9fd957a01a02facb57cad2378240
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-11-08 04:23:01 +0700

    Initialize error module

---
+++ smc/compiler/error.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class Error(Exception):
+  def __init__(self, error_code=None, token=None, message=None):
+    self.error_code = error_code
+    self.token = token
+    self.message = f'{self.__class__.__name__}: {message}'
+    pass
+
+class LexerError(Error):
+  pass
+
+class ParserError(Error):
+  pass
+
+class ErrorCode(Enum):
+  UNEXPECTED_TOKEN = 'Unexpected token'  
