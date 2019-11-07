cda2f6699721955d4820190d6f3fde6b7283cb3d
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-11-08 04:22:05 +0700

    Update mnemonic lists

--- smc/compiler/token.py
+++ smc/compiler/token.py
@@ -2,21 +2,37 @@ from enum import Enum
 
 class TokenType(Enum):
   # block of reserved words
+  ## R-Type
   ADD      = 'add'
   NAND     = 'nand'
+  ## I-Type
   LW       = 'lw'
   SW       = 'sw'
   BEQ      = 'beq'
+  ## J-Type
   JALR     = 'jalr'
+  ## O-Type
   HALT     = 'halt'
   NOOP     = 'noop'
+  ## Spcial Type
   FILL     = '.fill'  
+
   # misc
   INITIAL  = 'INITIAL'
   INTERGER = 'INTERGER'
   WORD     = 'WORD'
   EOL      = 'EOL' # End of line
   EOF      = 'EOF' # End of file
+
+  @staticmethod
+  def MNEMONIC_LISTS():
+    # mnemonic is same reserved keyword
+    tt_list = list(TokenType)
+    start_index = tt_list.index(TokenType.ADD)
+    end_index = tt_list.index(TokenType.FILL)
+    mnemonic_lists = [token_type for token_type in tt_list[start_index: end_index -1]]
+
+    return mnemonic_lists
 
 class Token(object):
   def __init__(self, type, value, line=None, column=None):
