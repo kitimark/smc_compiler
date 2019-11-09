90a5164f8e904a2b5e94712c740c227ae163f718
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-11-10 03:49:09 +0700

    Update binary object

--- tests/binary/test_binary.py
+++ tests/binary/test_binary.py
@@ -4,5 +4,5 @@ from smc.binary import Binary
 
 class BinaryTest(unittest.TestCase):
   def test_init(self):
-    actual = Binary(5, 32)
-    assert actual.__str__() == '00000000000000000000000000000101'
+    actual = Binary(auto=5, bits=32)
+    assert actual.bin == '00000000000000000000000000000101'
