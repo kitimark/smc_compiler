326f9465aaf93ed11377d70bae9caf89f2fcc455
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-11-10 01:03:57 +0700

    Fix error dont display message

--- smc/compiler/error.py
+++ smc/compiler/error.py
@@ -2,10 +2,11 @@ from enum import Enum
 
 class Error(Exception):
   def __init__(self, error_code=None, token=None, message=None):
+    message = f'{self.__class__.__name__}: {message}'
+    super().__init__(message)
     self.error_code = error_code
     self.token = token
-    self.message = f'{self.__class__.__name__}: {message}'
-    pass
+    self.message = message
 
 class LexerError(Error):
   pass
